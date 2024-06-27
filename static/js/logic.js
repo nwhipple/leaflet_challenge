23eee2d2b82c63b44d2cbf2227ab6ddf2f8ff691
nwhipple <66106887+nwhipple@users.noreply.github.com>
2024-06-27 10:36:18 -0700

    submission

---
+++ static/js/logic.js
@@ -0,0 +1,118 @@
+// Store our API endpoint as queryUrl.
+const queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+// let myMap = L.map("map", {
+//       center: [
+//         37.09, -95.71
+//       ],
+//       zoom: 5,
+//       // layers: [street, earthquakes]
+//     });
+//     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//   }).addTo(myMap);
+// Perform a GET request to the query URL/
+d3.json(queryUrl).then(function (data) {
+  // Once we get a response, send the data.features object to the createFeatures function.
+  // L.geoJSON(data.features).addTo(myMap);
+  createFeatures(data.features);
+});
+
+// The below code was from https://leafletjs.com/examples/choropleth/ ; the tutor helped me implement it
+function getColor(d) {
+  return d > 90 ? '#800026' :
+         d > 70  ? '#BD0026' :
+         d > 50  ? '#E31A1C' :
+         d > 30  ? '#FC4E2A' :
+         d > 10  ? '#FD8D3C' :
+                  '#FFEDA0';
+}
+
+function createFeatures(earthquakeData) {
+
+  // Define a function that we want to run once for each feature in the features array.
+  // Give each feature a popup that describes the place and time of the earthquake.
+  function onEachFeature_func(feature, layer) {
+    layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>Magnitude:${feature.properties.mag}</p><p>Depth: ${feature.geometry.coordinates[2]}</p>`);
+  }
+
+  // Create a GeoJSON layer that contains the features array on the earthquakeData object.
+  // Run the onEachFeature function once for each piece of data in the array.
+  let earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature_func,
+    pointToLayer: function (feature, latlng) {
+      return L.circleMarker(latlng, {
+        radius: feature.properties.mag * 5,
+        fillColor: getColor(feature.geometry.coordinates[2]),
+        color: "black",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+      });
+    }
+  });
+
+  // Send our earthquakes layer to the createMap function/
+  createMap(earthquakes);
+}
+
+function createMap(earthquakes) {
+
+  // Create the base layers.
+  let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  })
+
+  let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+  });
+
+  // Create a baseMaps object.
+  let baseMaps = {
+    "Street Map": street,
+    "Topographic Map": topo
+  };
+
+  // Create an overlay object to hold our overlay.
+  let overlayMaps = {
+    Earthquakes: earthquakes
+  };
+
+  // Create our map, giving it the streetmap and earthquakes layers to display on load.
+  let myMap = L.map("map", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 5,
+    layers: [street, earthquakes]
+  });
+
+  // Create a layer control.
+  // Pass it our baseMaps and overlayMaps.
+  // Add the layer control to the map.
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+
+
+  var legend = L.control({position: 'bottomright'});
+
+// The below code was from https://leafletjs.com/examples/choropleth/ ; the tutor helped me implement it
+
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [-10, 10, 30, 50, 70, 90];
+        // labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+legend.addTo(myMap);
+}
